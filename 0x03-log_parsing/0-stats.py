1d384ce9c0cf33c33f91f20b2efe4f145b7991ab
derrickkimtai <derrickmoio92@gmail.com>
2024-03-28 15:24:00 +0300

    readme

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,45 +1,34 @@
 #!/usr/bin/python3
-""" Log Parsing """
-
+"""Log parsing"""
 import sys
 
-status_codes = {
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
-    "403": 0,
-    "404": 0,
-    "405": 0,
-    "500": 0
-}
-
+status_codes = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
 file_size = 0
 counter = 0
 
+def print_stats():
+    print("File size: {}".format(file_size))
+    for code in sorted(status_codes.keys()):
+        if status_codes[code] > 0:
+            print("{}: {}".format(code, status_codes[code]))
+
 try:
     for line in sys.stdin:
+        try:
+            parts = line.split()
+            size = int(parts[-1])
+            code = parts[-2]
+            if code in status_codes:
+                status_codes[code] += 1
+                file_size += size
+        except:
+            continue
+
         counter += 1
-        data = line.split()
-        try:
-            file_size += int(data[-1])
-        except:
-            pass
-        try:
-            if data[-2] in status_codes:
-                status_codes[data[-2]] += 1
-        except:
-            pass
-        if counter == 10:
-            print("File size: {}".format(file_size))
-            for key, value in sorted(status_codes.items()):
-                if value != 0:
-                    print("{}: {}".format(key, value))
-            counter = 0
+        if counter % 10 == 0:
+            print_stats()
 except KeyboardInterrupt:
-    pass
+    print_stats()
+    raise
 finally:
-    print("File size: {}".format(file_size))
-    for key, value in sorted(status_codes.items()):
-        if value != 0:
-            print("{}: {}".format(key, value))
+    print_stats()
