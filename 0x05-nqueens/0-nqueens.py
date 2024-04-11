f38b62eac507194b9483aacbe1494003c27d52de
derrickkimtai <derrickmoio92@gmail.com>
2024-04-11 21:12:30 +0300

    nqeens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -3,7 +3,6 @@
 import sys
 
 def is_safe(board, row, col, N):
-    # Check if there is a queen in the same column
     for i in range(row):
         if board[i] == col or \
             board[i] - i == col - row or \
@@ -12,49 +11,30 @@ def is_safe(board, row, col, N):
     return True
 
 def solve_nqueens(N):
+    if not N.isdigit():
+        print("N must be a number")
+        sys.exit(1)
+    N = int(N)
     if N < 4:
         print("N must be at least 4")
         sys.exit(1)
-    
-    solutions = []
 
-    def solve_util(board, row):
+    def solve(board, row, N, result):
         if row == N:
-            solutions.append(board[:])
+            result.append(board[:])
             return
-
         for col in range(N):
             if is_safe(board, row, col, N):
                 board[row] = col
-                solve_util(board, row + 1)
+                solve(board, row + 1, N, result)
 
-    solve_util([0] * N, 0)
+    result = []
+    solve([-1] * N, 0, N, result)
+    for solution in result:
+        print(' '.join(str(col + 1) for col in solution))
 
-    return solutions
-def print_solution(solution):
-    for i in range(len(solution)):
-        print([(i, solution[i]) for i in range(len(solution))])
-
-
-def main():
+if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-
-    try:
-        N = int(sys.argv[1])
-    except ValueError:
-        print("N must be a number")
-        sys.exit(1)
-
-    if N < 4:
-        print("N must be at least 4")
-        sys.exit(1)
-
-    solutions = solve_nqueens(N)
-    for solution in solutions:
-        print_solution(solution)
-        print()
-
-if __name__ == "__main__":
-    main()
+    solve_nqueens(sys.argv[1])
