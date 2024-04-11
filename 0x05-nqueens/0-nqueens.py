a2346bfc6850ee7d42cdf412447739e39455bf90
derrickkimtai <derrickmoio92@gmail.com>
2024-04-11 22:38:40 +0300

    queens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,40 +1,43 @@
 #!/usr/bin/python3
+"""N queens"""
 
 import sys
 
-def is_safe(board, row, col, N):
-    for i in range(row):
-        if board[i] == col or \
-            board[i] - i == col - row or \
-            board[i] + i == col + row:
+def isSafe(board, row, col):
+    """Check if a queen can be placed on board[row][col]"""
+    for i in range(col):
+        if board[i] == row or board[i] == row - col + i or board[i] == row + col - i:
             return False
     return True
 
-def solve_nqueens(N):
-    if not N.isdigit():
-        print("N must be a number")
-        sys.exit(1)
-    N = int(N)
-    if N < 4:
-        print("N must be at least 4")
-        sys.exit(1)
+def solveNQUtil(board, col):
+    """Solve N queens"""
+    n = len(board)
+    if col == n:
+        print(str([[i, board[i]] for i in range(n)]))
+        return
+    for i in range(n):
+        if isSafe(board, i, col):
+            board[col] = i
+            solveNQUtil(board, col + 1)
+            board[col] = -1
 
-    def solve(board, row, N, result):
-        if row == N:
-            result.append(board[:])
-            return
-        for col in range(N):
-            if is_safe(board, row, col, N):
-                board[row] = col
-                solve(board, row + 1, N, result)
-
-    result = []
-    solve([-1] * N, 0, N, result)
-    for solution in result:
-        print(' '.join(str(col + 1) for col in solution))
+def solveNQ(n):
+    """Solve N queens"""
+    board = [-1 for i in range(n)]
+    solveNQUtil(board, 0)
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-    solve_nqueens(sys.argv[1])
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+    solveNQ(n)
+    
