9b4dc96e4b75e6ddb1f2aedb9c4c8f36a87d911f
derrickkimtai <derrickmoio92@gmail.com>
2024-04-19 04:28:41 +0300

    fun

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,35 +2,45 @@
 
 const request = require('request');
 
-function printMovieCharacters (movieId) {
-  const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
-
-  request(url, function (error, response, body) {
-    if (error) {
-      console.error('Error:', error);
-      return;
-    }
-
-    const data = JSON.parse(body);
-    const characters = data.characters;
-
-    if (characters) {
-      characters.forEach((characterUrl) => {
-        request(characterUrl, function (error, response, body) {
-          if (error) {
-            console.error('Error:', error);
-            return;
-          }
-
-          const characterName = JSON.parse(body).name;
-          console.log(characterName);
-        });
-      });
-    } else {
-      console.error('No characters found for this movie');
-    }
-  });
+// Check if a movie ID was provided
+if (process.argv.length < 3) {
+  console.log('Please provide a movie ID as an argument.');
+  process.exit(1);
 }
 
-const movieId = process.argv[2];
-printMovieCharacters(movieId);
+const movieId = process.argv[2]; // Get the movie ID from the command-line argument
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`; // Construct the URL for the movie
+
+request(url, { json: true }, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
+
+  if (response.statusCode !== 200) {
+    console.error('Error:', response.statusCode);
+    return;
+  }
+
+  if (body.characters && body.characters.length > 0) {
+    body.characters.forEach(characterUrl => {
+      // Extract the character ID from the URL
+      const characterId = characterUrl.match(/\/api\/people\/(\d+)\//)[1];
+      request(`https://swapi.dev/api/people/${characterId}/`, { json: true }, (error, response, body) => {
+        if (error) {
+          console.error('Error:', error);
+          return;
+        }
+
+        if (response.statusCode !== 200) {
+          console.error('Error:', response.statusCode);
+          return;
+        }
+
+        console.log(body.name); // Print the character's name
+      });
+    });
+  } else {
+    console.log('No characters found for this movie.');
+  }
+});
