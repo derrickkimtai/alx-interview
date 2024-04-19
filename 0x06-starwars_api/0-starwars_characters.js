85046e4909b2aaab816b455c0926378d80a79f73
derrickkimtai <derrickmoio92@gmail.com>
2024-04-19 04:35:16 +0300

    fun

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,46 +1,17 @@
 #!/usr/bin/node
 
-const request = require('request');
+const util = require('util');
+const request = util.promisify(require('request'));
+const movie = process.argv[2];
 
-// Check if a movie ID was provided
-if (process.argv.length < 3) {
-  console.log('Please provide a movie ID as an argument.');
-  process.exit(1);
+async function getCharacter (movie) {
+  const url = 'https://swapi-api.hbtn.io/api/films/' + movie;
+  const response = await request(url);
+  const characters = JSON.parse(response.body).characters;
+  for (const character of characters) {
+    const res = await request(character);
+    console.log(JSON.parse(res.body).name);
+  }
 }
 
-const movieId = process.argv[2]; // Get the movie ID from the command-line argument
-const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`; // Construct the URL for the movie
-
-request(url, { json: true }, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-    return;
-  }
-
-  if (response.statusCode !== 200) {
-    console.error('Error:', response.statusCode);
-    return;
-  }
-
-  if (body.characters && body.characters.length > 0) {
-    body.characters.forEach(characterUrl => {
-      // Extract the character ID from the URL
-      const characterId = characterUrl.match(/\/api\/people\/(\d+)\//)[1];
-      request(`https://swapi.dev/api/people/${characterId}/`, { json: true }, (error, response, body) => {
-        if (error) {
-          console.error('Error:', error);
-          return;
-        }
-
-        if (response.statusCode !== 200) {
-          console.error('Error:', response.statusCode);
-          return;
-        }
-
-        console.log(body.name); // Print the character's name
-      });
-    });
-  } else {
-    console.log('No characters found for this movie.');
-  }
-});
+getCharacter(movie);
