76b09fbca97fcdd88eb4343751f55dc219095d06
derrickkimtai <derrickmoio92@gmail.com>
2024-04-19 04:17:50 +0300

    stara war

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,13 +1,36 @@
 #!/usr/bin/node
 
 const request = require('request');
-const url = 'https://swapi-api.alx-tools.com/api/' + process.argv[2];
 
-request(url, async function (error, response, body) {
-  if (error) {
-    console.error(error);
-  } else {
-    console.log(JSON.parse(body).name);
-  }
+function printMovieCharacters(movieId) {
+  const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+
+  request(url, function (error, response, body) {
+    if (error) {
+      console.error('Error:', error);
+      return;
+    }
+
+    const data = JSON.parse(body);
+    const characters = data.characters;
+
+    if (characters) {
+      characters.forEach((characterUrl) => {
+        request(characterUrl, function (error, response, body) {
+          if (error) {
+            console.error('Error:', error);
+            return;
+          }
+
+          const characterName = JSON.parse(body).name;
+          console.log(characterName);
+        });
+      });
+    } else {
+      console.error('No characters found for this movie');
+    }
+  });
 }
-);
+
+const movieId = process.argv[2];
+printMovieCharacters(movieId);
