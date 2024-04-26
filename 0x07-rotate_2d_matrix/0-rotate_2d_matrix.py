6d47cc9c2c677d382a424db09875d4a7a41fdf29
Alogarithm_Ke <derrickmoio92@gmail.com>
2024-04-26 03:06:53 +0300

    2d

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Rotates a 2D matrix 90 degrees clockwise"""
+def rotate_2d_matrix(matrix):
+    """Rotates a 2D matrix 90 degrees clockwise"""
+    n = len(matrix)
+    for x in range(n // 2):
+        for y in range(x, n - x - 1):
+            temp = matrix[x][y]
+            matrix[x][y] = matrix[n - 1 - y][x]
+            matrix[n - 1 - y][x] = matrix[n - 1 - x][n - 1 - y]
+            matrix[n - 1 - x][n - 1 - y] = matrix[y][n - 1 - x]
+            matrix[y][n - 1 - x] = temp
+    return matrix
