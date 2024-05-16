b74c16b3331d4ea09366423159f9c2580e407280
Alogarithm_Ke <derrickmoio92@gmail.com>
2024-05-16 22:41:18 +0300

    interview

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,19 +1,27 @@
 #!/usr/bin/python3
-"""prime game"""
+"""
+Prime Game
+"""
+
 
 def isWinner(x, nums):
-    """prime game"""
+    """
+    Prime Game
+    """
     maria_wins = 0
     ben_wins = 0
-    for n in nums:
-        prime_count = sum([1 for i in range(1, n + 1) if n % i == 0])
-        if prime_count == 2:
-            maria_wins += 1
-        else:
+
+    for num in nums:
+        prime_count = sum(1 for i in range(2, num + 1) if all(i % j != 0 for j in range(2, int(i ** 0.5) + 1)))
+
+        if prime_count % 2 == 0:
             ben_wins += 1
+        else:
+            maria_wins += 1
 
     if maria_wins > ben_wins:
-        return "Ben"
-    if maria_wins < ben_wins:
-        return "Maria"
-    return None
+        return 'Maria'
+    elif maria_wins < ben_wins:
+        return 'Ben'
+    else:
+        return None
