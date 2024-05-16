ad2f2da5590cd261c9218cbef7369e0c5c57b940
Alogarithm_Ke <derrickmoio92@gmail.com>
2024-05-16 20:09:54 +0300

    interview

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,27 +2,33 @@
 """prime game"""
 
 def isWinner(x, nums):
-    """is winer"""
+    """prime game"""
     if not nums or x < 1:
         return None
-
-    max_num = max(nums)
-    sieve = [1] * (max_num + 1)
-    sieve[0] = sieve[1] = 0
-    for i in range(2, int(max_num ** 0.5) + 1):
-        if sieve[i] == 1:
-            for j in range(i * i, max_num + 1, i):
-                sieve[j] = 0
-    primes = [i for i in range(max_num + 1) if sieve[i] == 1]
-
-    wins_for_maria = 0
-    for num in nums:
-        if num in primes:
-            wins_for_maria += 1
-
-    if wins_for_maria == 0:
+    n = max(nums)
+    sieve = [True for _ in range(max(n + 1, 2))]
+    sieve[0:2] = [False, False]
+    for i in range(2, int(n ** 0.5) + 1):
+        if not sieve[i]:
+            continue
+        for j in range(i * i, n + 1, i):
+            sieve[j] = False
+    sieve = [i for i, j in enumerate(sieve) if j]
+    m = len(sieve)
+    scores = {1: 0, 2: 1}
+    for i in range(3, n + 1):
+        scores[i] = 0
+        for j in range(m):
+            if sieve[j] > i:
+                break
+            if i % sieve[j] == 0 and scores[i - sieve[j]] == 0:
+                scores[i] = 1
+                break
+    player1 = 0
+    for i in nums:
+        player1 += scores[i]
+    if player1 * 2 == len(nums):
+        return None
+    if player1 * 2 < len(nums):
         return "Ben"
-    elif wins_for_maria % 2 == 0:
-        return "Maria"
-    else:
-        return "Ben"
+    return "Maria"
