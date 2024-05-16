8d1ad06133d918761fed11636acfaa47e3ef38c6
Alogarithm_Ke <derrickmoio92@gmail.com>
2024-05-16 20:13:36 +0300

    prime

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -3,32 +3,17 @@
 
 def isWinner(x, nums):
     """prime game"""
-    if not nums or x < 1:
-        return None
-    n = max(nums)
-    sieve = [True for _ in range(max(n + 1, 2))]
-    sieve[0:2] = [False, False]
-    for i in range(2, int(n ** 0.5) + 1):
-        if not sieve[i]:
-            continue
-        for j in range(i * i, n + 1, i):
-            sieve[j] = False
-    sieve = [i for i, j in enumerate(sieve) if j]
-    m = len(sieve)
-    scores = {1: 0, 2: 1}
-    for i in range(3, n + 1):
-        scores[i] = 0
-        for j in range(m):
-            if sieve[j] > i:
-                break
-            if i % sieve[j] == 0 and scores[i - sieve[j]] == 0:
-                scores[i] = 1
-                break
-    player1 = 0
-    for i in nums:
-        player1 += scores[i]
-    if player1 * 2 == len(nums):
-        return None
-    if player1 * 2 < len(nums):
+    maria_wins = 0
+    ben_wins = 0
+    for n in nums:
+        prime_count = sum([1 for i in range(1, n + 1) if n % i == 0])
+        if prime_count == 2:
+            maria_wins += 1
+        else:
+            ben_wins += 1
+
+    if maria_wins > ben_wins:
         return "Ben"
-    return "Maria"
+    if maria_wins < ben_wins:
+        return "Maria"
+    return None
